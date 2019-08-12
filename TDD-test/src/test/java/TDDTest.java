111b70487b7fd3f26ed0f2231317cf8520dd9e4d
Ethan <ethan.huang@oocl.com>
2019-08-12 14:37:39 +0800

    test: add test about given two equal level poker which is 2C3H4C7H7C and 2D3S4D7S7D, but not implement still

--- TDD-test/src/test/java/TDDTest.java
+++ TDD-test/src/test/java/TDDTest.java
@@ -74,6 +74,14 @@ public class TDDTest {
         Assert.assertEquals(pokerList2,pokerApp.playgame(pokerList1,pokerList2,5));
     }
 
+    @Test
+    public void should_return_null_when_call_playgame_function_given_2C3H4C7H7C_and_2D3S4D7S7D(){
+
+        List<Poker> pokerList1 = createPokerList(new Poker(2,"C"),new Poker(3,"H"),new Poker(4,"C"),new Poker(7,"H"),new Poker(7,"C"));
+        List<Poker> pokerList2 = createPokerList(new Poker(2,"D"),new Poker(3,"S"),new Poker(4,"D"),new Poker(7,"S"),new Poker(7,"D"));
+        Assert.assertEquals(null,pokerApp.playgame(pokerList1,pokerList2,5));
+    }
+
     private List<Poker> createPokerList(Poker poker1,Poker poker2,Poker poker3,Poker poker4,Poker poker5){
         List<Poker> pokerList = new ArrayList<>();
         pokerList.add(poker1);

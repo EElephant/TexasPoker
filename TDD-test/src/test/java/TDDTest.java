590b4084686b782d8f5342d8bc4627e486d33867
Ethan <ethan.huang@oocl.com>
2019-08-12 13:27:07 +0800

    test: pass test about given two equal level poker which is 3D3H and 3C3S

--- TDD-test/src/test/java/TDDTest.java
+++ TDD-test/src/test/java/TDDTest.java
@@ -40,6 +40,20 @@ public class TDDTest {
         Assert.assertEquals(pokerList2,pokerApp.playgame(pokerList1,pokerList2,2));
     }
 
+    @Test
+    public void should_return_null_when_call_playgame_function_given_3D3H_and_2D3H(){
+
+//        List<Poker> pokerList1 = createPokerList(new Poker(2,"D"),new Poker(3,"D"),new Poker(4,"D"),new Poker(5,"D"),new Poker(6,"D"));
+//        List<Poker> pokerList2 = createPokerList(new Poker(2,"D"),new Poker(3,"D"),new Poker(4,"D"),new Poker(5,"D"),new Poker(7,"D"));
+        List<Poker> pokerList1 = new ArrayList<>();
+        List<Poker> pokerList2 = new ArrayList<>();
+        pokerList1.add(new Poker(3,"D"));
+        pokerList1.add(new Poker(3,"H"));
+        pokerList2.add(new Poker(3,"C"));
+        pokerList2.add(new Poker(3,"S"));
+        Assert.assertEquals(null,pokerApp.playgame(pokerList1,pokerList2,2));
+    }
+
 
 
     private List<Poker> createPokerList(Poker poker1,Poker poker2,Poker poker3,Poker poker4,Poker poker5){
